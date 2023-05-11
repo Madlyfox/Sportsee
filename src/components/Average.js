22f2c7826a18486468c3b202796688260c07a1b0
Borromée Stanley <48676332+Madlyfox@users.noreply.github.com>
2023-05-11 15:31:15 +0200

    fix #1

--- src/components/Average.js
+++ src/components/Average.js
@@ -4,51 +4,6 @@ import { useParams } from "react-router-dom";
 import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 import "../scss/average.scss";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 const Average = () => {
   const userId = useParams();
 
@@ -60,8 +15,6 @@ const Average = () => {
   let data;
 
   if (!fetchResult === false) {
-    console.log(fetchResult);
-
     data = fetchResult;
 
     function formatYAxis(value, index) {
@@ -74,6 +27,18 @@ const Average = () => {
       if (index === 6) return "D";
       return index;
     }
+
+    const CustomTooltip = ({ active, payload, label }) => {
+      if (active && payload && payload.length) {
+        return (
+          <div className="custom-tooltip">
+            <small className>{`${payload[0].value} min`}</small>
+          </div>
+        );
+      }
+
+      return null;
+    };
 
     return (
       <ResponsiveContainer width="100%" height="80%">
@@ -95,7 +60,7 @@ const Average = () => {
             tickFormatter={formatYAxis}
           />
 
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           <Line
             type="monotone"
             dataKey="sessionLength"
